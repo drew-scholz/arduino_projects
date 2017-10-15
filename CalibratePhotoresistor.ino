462efda6afb55e03e13ec98b54d25261e972ecca
Drew Scholz <arscholz@eagles.ewu.edu>
2017-10-15 00:51:56 -0700

    Add files via upload

---
+++ CalibratePhotoresistor.ino
@@ -0,0 +1,184 @@
+#define LED_MIN 0
+#define LED_HALF 128
+#define LED_MAX 255
+#define LED_PIN 9
+#define ANALOG_IN A0
+
+#define NUM_AVG 20 
+
+int ara[256] = {-1};
+int SETTLING_TIME;
+int MIN_READING,MAX_READING;
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(ANALOG_IN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    /* wait for user input to start*/
+  Serial.println("\nPress a key and enter to continue");
+  while (Serial.available()<=0);
+  while(Serial.available()>0) Serial.read();
+
+  /* delay for 3 seconds */
+  delay(3000);
+
+  /* Step 1: Serial Plotter, adjust NUM_AVG for testing */
+  //findAvg();
+  
+  /* Step 2: */
+  findSettlingTime();
+  
+  /* Step 3: */
+  findPhotoMinMax(); 
+  
+  /* Step 4,5,6: */
+  fillAra();
+  linearGuessLoop();
+
+  /* Step 7: */ 
+  testLoop();
+}
+
+void findAvg() {
+  int total = 0;
+  int numReadings = 500;
+  while(total < numReadings) {
+    getAverage();
+    total++;
+  }
+}
+
+int getAverage() {
+  int total = 0;
+  
+  for(int i = 0; i < NUM_AVG; i++) {
+    total += analogRead(ANALOG_IN);   
+  }
+  int avg = total/NUM_AVG;
+  
+  return avg;
+}
+
+void fillAra() {
+  for(int i = 0; i < 256; i++) {
+    ara[i] = -1;
+  }
+}
+
+void findSettlingTime() {
+  unsigned long startMillis, endMillis, settlingTime;
+
+  startMillis = millis();
+  
+  analogWrite(LED_PIN,LED_HALF);
+  readLight();
+  
+  endMillis = millis();
+  
+  settlingTime = endMillis - startMillis;
+  Serial.print("settling time (msec) = ");
+  Serial.println(settlingTime);
+
+  SETTLING_TIME = settlingTime;
+}
+
+int readLight(){
+  int prev = 0;
+  int cur = 1;
+  
+  while(prev != cur) {
+    prev = cur;
+    cur = getAverage();
+  }
+
+  return prev;
+}
+
+void findPhotoMinMax() { 
+  analogWrite(LED_PIN,LED_MIN);
+  delay(SETTLING_TIME);
+  MIN_READING = readLight();
+
+  analogWrite(LED_PIN,LED_MAX);
+  delay(SETTLING_TIME);
+  MAX_READING = readLight(); 
+  
+  Serial.print("smallest = ");
+  Serial.println(MIN_READING);  
+  Serial.print("largest = ");
+  Serial.println(MAX_READING);
+}
+
+void linearGuessLoop() {
+  int total, avg;
+
+  Serial.println("\nLinear guess loop:");
+
+  //increment LED brightness from 0 to 255
+  for(int i = 0; i < 256; i++) {
+    analogWrite(LED_PIN, i);
+    //wait for photoresistor to settle
+    delay(SETTLING_TIME);
+
+    // take average reading
+    avg = getAverage();
+
+    //map reading from the min and max
+    int mapReading = map(avg,MIN_READING,MAX_READING,0,255);
+    mapReading = constrain(mapReading,0,255);
+
+    //store map reading to array
+    ara[mapReading] = i ;
+
+    Serial.print("LED=");
+    Serial.print(i);  
+    Serial.print("\tLight=");
+    Serial.print(avg); 
+    Serial.print("\tLinMapped=");
+    Serial.println(mapReading);  
+  }
+  
+  // check array for -1 entries
+  int lastGood = 0;
+  for(int i = 0; i < 256; i++) {
+    if(ara[i] == -1) 
+      ara[i] = lastGood;
+     else
+       lastGood = ara[i];
+  }
+  return ara;
+} 
+
+void testLoop() {
+  int total, avg;
+
+  Serial.println("\ntest loop");
+
+  //increment LED brightness from 0 to 255
+  for(int i = 0; i < 256; i++) {
+    analogWrite(LED_PIN, i);
+    //wait for photoresistor to settle
+    delay(SETTLING_TIME);
+
+    // take average reading
+    avg = getAverage();
+    
+    int mapReading = map(avg,MIN_READING,MAX_READING,0,255);
+    mapReading = constrain(mapReading,0,255);
+    
+    Serial.print("LED=");
+    Serial.print(i); 
+    Serial.print("\tLight=");
+    Serial.print(avg);  
+    Serial.print("\tReverseMapped="); 
+    Serial.println(ara[mapReading]);
+  }
+}
+
+
+
---
+++ ReadRemoteControl.ino
@@ -0,0 +1,150 @@
+/*
+ * an Arduino program that recognizes (these) transmissions 
+ * and prints the address and command bytes, in hexadecimal,
+ * to the serial monitor, one line per pair. 
+ * When a repeat code is received,
+ * it should print the string REPEAT on its own line.
+ * 
+ */
+#define IR_INPUT 8
+#define FALSE 0
+#define CMD_SEQ 1
+#define REP_SEQ 2
+#define BYTE_SIZE 8
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IR_INPUT, INPUT);
+  
+  Serial.println("please press a button on the remote");
+  Serial.println("===================================");
+  
+  timer1Init(); 
+}
+
+void timer1Init()
+{
+  TCCR1A = 0x00;
+  TCCR1B = 0x05; // 16 Mhz / 1024 = 15.625 kHz --> 64 usec per count
+  TCCR1C = 0x00;
+  TIMSK1 = 0x00; // not using interrupt
+  TCNT1 = 0x00; // initial count of 0
+}
+
+void loop() {
+  int address = 0x00;
+  int cmd = 0x00;
+  int addressComplement = 0x00;
+  int cmdComplement = 0x00; 
+  
+  int sequence = startSequenceFound();
+  
+  if(sequence == CMD_SEQ) 
+  {   
+        address = readByte(address);  
+        addressComplement = readByte(addressComplement);
+        cmd = readByte(cmd);
+        cmdComplement = readByte(cmdComplement);
+        
+        Serial.print("address: 0x");
+        Serial.println(address, HEX);
+        if(address + addressComplement != 255)
+        {   
+            Serial.println("Warning!: check byte is not the inverse of the address");
+            Serial.print("complement: 0x");
+            Serial.println(addressComplement, HEX);
+        }
+        
+        Serial.print("cmd: 0x");
+        Serial.println(cmd, HEX);        
+        if(cmd + cmdComplement != 255)
+        {   
+            Serial.println("Warning!: check byte is not the inverse of the command");
+            Serial.print("complement: 0x");
+            Serial.println(cmdComplement, HEX);
+        }
+
+        Serial.println("========================");
+        delay(200);
+  }
+  else if(sequence == REP_SEQ)
+  {
+    Serial.println("REPEAT sequence recieved");
+    Serial.println("========================");  
+  }
+}
+
+int startSequenceFound() { 
+  int countActiveLength = 0;
+  int countInactiveLength = 0;
+  
+  if(digitalRead(IR_INPUT) == LOW)
+  {     
+    /* measure the active length*/
+    TCNT1 = 0;  
+    while(digitalRead(IR_INPUT)!= HIGH){}
+    countActiveLength = TCNT1;
+    TCNT1 = 0;
+
+    if(countActiveLength > 120 && countActiveLength < 160) // 9 msec = 140 counts
+    {  
+      
+      /* measure the inactive length */
+      while(digitalRead(IR_INPUT)!= LOW){} 
+      countInactiveLength = TCNT1;
+   
+      if(countInactiveLength > 65 && countInactiveLength < 85) // 4.5 msec = 70 counts
+      {
+          return CMD_SEQ;
+      }
+      else if (countInactiveLength > 25 && countInactiveLength < 45 ) // 2.25 msec = 35 counts
+      {
+          return REP_SEQ;
+      }
+      else
+      {
+        Serial.println("start signal NOT recieved");
+      }
+    }
+  }
+  
+  return FALSE;
+}
+
+
+int readByte(int code)
+{
+    int abit;
+    
+    for(int i = 0; i < BYTE_SIZE; i++)
+    {
+      abit = readBit();
+      if(abit != -1)
+        code = code | (abit << i);    
+    }
+    
+    return code;
+}
+
+int readBit()
+{  
+    int bitLength = 0;
+
+    // wait until bit goes HIGH
+    while(digitalRead(IR_INPUT) != HIGH){}
+    TCNT1 = 0;
+
+    // wait until bit goes LOW
+    while(digitalRead(IR_INPUT)!= LOW){}
+    bitLength = TCNT1;
+
+    if(bitLength > 6 && bitLength < 12) // 562.5 usec = 9 counts 
+      return 0; 
+ 
+    else if(bitLength > 20 && bitLength < 35) // 1687.5 usec = 26 counts
+      return 1;
+      
+    else
+      return -1;
+}
+
